b02f6f669b536d789b019339426d39a2637f0d98
RicardoGoncalo <ri.a.p.goncalo@gmail.com>
2026-09-02 12:49:19 +0100

    Leetcode: exercise 1115: resolved exercise

--- leetcode/1115/c++/main.cpp
+++ leetcode/1115/c++/main.cpp
@@ -1,7 +1,9 @@
 class FooBar {
-
 private:
     int n;
+    mutex mtx;
+    condition_variable cv; // notifies change with notify_all
+    bool fooTurn = true;
 
 public:
     FooBar(int n) {
@@ -12,17 +14,33 @@ public:
 
         for (int i = 0; i < n; i++) {
 
-        	// printFoo() outputs "foo". Do not change or remove this line.
-        	printFoo();
+            unique_lock<mutex> lock(mtx); // create a variable named loc of type unique_lock<mutex> with value = mtx
+
+            cv.wait(lock, [this]() { // wait using the conditional variable. When it is notified, check the predicate if it should continue
+                return fooTurn; //this is part of the lambda function [this](){return fooTurn}, with [this] being the lambda capture list, copying this first
+            });
+
+            // printFoo() outputs "foo".
+            printFoo();
+
+            fooTurn = false;
+            cv.notify_all();
         }
     }
 
     void bar(function<void()> printBar) {
-
         for (int i = 0; i < n; i++) {
+            unique_lock<mutex> lock(mtx);
 
-        	// printBar() outputs "bar". Do not change or remove this line.
-        	printBar();
+            cv.wait(lock, [this]() {
+                return !fooTurn;
+            });
+
+            // printBar() outputs "bar".
+            printBar();
+
+            fooTurn = true;
+            cv.notify_all();
         }
     }
 };
